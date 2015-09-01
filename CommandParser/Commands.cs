582f86f8f5c9e987fc15830966082d57d873fd6c
Iryna Kushnir <irynakushnir@Irynas-MacBook-Pro.local>
2015-09-02 01:08:48 +0300

    Some logic change for -k command

--- CommandParser/Commands.cs
+++ CommandParser/Commands.cs
@@ -10,15 +10,15 @@ namespace CommandParser
     {
         public static List<string> AwailableCommands = new List<string>{"/?", "/help", "-help", "-k", "-ping", "-print"}; 
 
-        public static void Ping()
+		public static void ShowHelp()
+		{
+			Console.WriteLine("Help!!!!!!");
+		}
+
+		public static void Ping()
         {
             Console.WriteLine("Pinging...");
             Console.Beep();
-        }
-
-        public static void ShowHelp()
-        {
-            Console.WriteLine("Help!!!!!!");
         }
 
         public static void Print(string message)
@@ -30,21 +30,18 @@ namespace CommandParser
         }
 
         public static void PrintKeyValue(string [] keysAndValues)
-        {
-            int l = keysAndValues.Length;
-            if (l == 0)
-            {
-				Console.WriteLine("You didn't specify keys and values for '-k' command. Use CommandParser.exe /? to see user help.");
-            }
-            else 
-            {
-                for (int i = 0; i <= (l%2==0? l-2:l-3); i += 2)
-                {
-                    Console.WriteLine("{0} - {1}", keysAndValues[i], keysAndValues[i + 1]);
-                }
-                if (l%2 == 1) Console.WriteLine("{0} - null", keysAndValues[l-1]);
-            }
-            
-        }
+		{
+			int l = keysAndValues.Length;
+			if (l == 0) {
+				Console.WriteLine ("No keys and values");
+			} else {
+				for (int i = 0; i <= (l % 2 == 0 ? l - 2 : l - 3); i += 2) {
+					Console.WriteLine ("{0} - {1}", keysAndValues [i], keysAndValues [i + 1]);
+				}
+				if (l % 2 == 1)
+					Console.WriteLine ("{0} - null", keysAndValues [l - 1]);
+                       
+			}
+		}
     }
 }
