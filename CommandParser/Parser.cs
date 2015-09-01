f511d667d1f6297b013fb294d3c8561f166b9488
unknown <irk@pc-irk.kyiv.ciklum.net>
2015-09-01 18:34:54 +0300

    Adding basic functionality for parser

--- CommandParser/Parser.cs
+++ CommandParser/Parser.cs
@@ -11,10 +11,47 @@ namespace CommandParser
         public static void ParseArgsAndExecuteCommands(string [] args)
         {
             if (args.Length == 0) Commands.ShowHelp();
-            if (args.Length == 1)
+            if (args.Length == 1 && (args[0] == "/?" || args[0] == "/help" || args[0] == "-help"))
             {
+                Commands.ShowHelp();
+            }
+            for (int i = 0; i < args.Length; i++)
+            {
+
+                if (!Commands.AwailableCommands.Contains(args[i])) {Console.WriteLine("'{0}' command is not supported, use CommandParser.exe /? to see set of allowed commands", args[i]); continue;}
+                switch (args[i])
+                {
+                    case "-ping": {Commands.Ping(); break;}
+                    case "-print":
+                    {
+                        if (i + 1 == args.Length || i + 1 == FindIndexOfNextCommand(i + 1, args)) Commands.Print(null);
+                        else
+                        {
+                            i++;
+                            Commands.Print(args[i]);
+                        }
+                        break;
+                    }
+                    case "-k":
+                    {
+                        int nextCommandIndex = FindIndexOfNextCommand(i + 1, args);
+                        Commands.PrintKeyValue(new List<string>(args).GetRange(i + 1, nextCommandIndex - i - 1).ToArray());
+                        i = nextCommandIndex - 1;
+                        break;
+                    }
+                }
+    
                 
             }
+        }
+
+        public static int FindIndexOfNextCommand(int startIndex, string[] args)
+        {
+            for (int i = startIndex; i < args.Length; i++)
+            {
+                if (Commands.AwailableCommands.Contains(args[i])) return i;
+            }
+            return args.Length;
         }
     }
 }
--- CommandParser/Properties/AssemblyInfo.cs
+++ CommandParser/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("CommandParser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ciklum")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("CommandParser")]
 [assembly: AssemblyCopyright("Copyright © ciklum 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
